a70bd6d2726bb4157059889157f64f8a86826a82
gertyboy1200 <90010253+gertyboy1200@users.noreply.github.com>
2023-05-09 15:45:59 -0600

    update

--- forests.cpp
+++ forests.cpp
@@ -1,14 +1,88 @@
 #include <iostream>
-#include <algorithm>
+#include <fstream>
+
 
 using namespace std;
 
-int main() {
+void printMatrix(int fullMattrix[][5]);
 
+int main(){
+    int people, trees, person, tree;
+    
+
+    int fullMatrix[people][trees];
+    while(cin >> person >> tree){
+        fullMatrix[person][tree] = 1;
+    }
+    printMatrix(fullMatrix);
+
+    cout << "program did not exit";
+
+
+
+}
+
+
+void printMatrix(int fullMattrix[][5]){
+    for (int i = 0; i < 4; i++){
+        for (int j = 0; j < 5; j++)
+            cout << fullMattrix[i][j] << " ";
+        cout << endl;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+int main() {
+    //system("clear");
 int people, trees;
+double total;
+
 cin >> people  >> trees;
-int matrix[people][trees];
-int totals[people];
+
+double matrix[people][trees];
+double totals[people];
 
 
 for(int n = 0; n < people; n++){
@@ -16,6 +90,7 @@ for(int n = 0; n < people; n++){
         matrix[n][m] = 0;
     }
 }
+/*
 cout << endl;
 for(int n = 0; n < people; n++){
     for(int m = 0; m < trees; m++){
@@ -24,18 +99,24 @@ for(int n = 0; n < people; n++){
     cout << endl;
 }
 
-//int treeArr[trees];
-
 int i, j;
 
 while(cin >> i >> j) {
     matrix[i-1][j-1] = j;
 }
 
-for (int i = 0 ; i < people; i++)
-    sort(matrix[i], matrix[i] + people);
+for(int f = 0; f < people; f++) {
+    total = 1;
+    for (int g = 0 ; g < trees; g++){
+        if(matrix[f][g] != 0){
+            total = total / matrix[f][g];
+        }
+    }
+    totals[f] = total;
+}
 
-
+*/
+/*
 for(int n = 0; n < people; n++){
     for(int m = 0; m < trees; m++){
         cout << matrix[n][m] << " ";
@@ -43,19 +124,37 @@ for(int n = 0; n < people; n++){
     cout << endl;
 }
 
+for(int a= 0; a < people; a++){
+    cout << totals[a] << " ";
+}
+
+int res = 1;
+
+    for (int i = 1; i < people; i++) {
+        int j = 0;
+        for (j = 0; j < i; j++)
+            if (totals[i] == totals[j])
+                break;
+        if (i == j)
+            res++;
+    }
+
+    cout << endl << res << endl;
+*/
 
 
-
-
+/*
 for(int n = 0; n < people; n++){
     for(int m = 0; m < trees; m++){
         totals[n] = totals[n] + matrix[n][m];
     }
 }
+
+
 /*
 for(int k = 0; k < people; k++)
 cout << totals[k] << endl;
-*/
+
 
 
 
@@ -65,6 +164,7 @@ cout << totals[k] << endl;
     
 //}
 
-
     return 0;
 }
+
+*/
