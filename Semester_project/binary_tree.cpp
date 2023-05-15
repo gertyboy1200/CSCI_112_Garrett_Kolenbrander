2622e266c11fcd08bca913a091645d7a51fe6387
gertyboy1200 <90010253+gertyboy1200@users.noreply.github.com>
2023-05-14 21:34:01 -0600

    update

--- Semester_project/binary_tree.cpp
+++ Semester_project/binary_tree.cpp
@@ -110,7 +110,7 @@ void traverse(Node* root) {
                 cout << "-1";
                 break;
             case 0:
-                cout << "Welcome to the forest";
+                cout << "You’re walking along a path and you approach the edge of a forest. There are two paths ahead of you, one leads into the forest and the other goes to the right along the edge of the trees.";
                 break;
             case 1:
                 cout << "1";
@@ -122,13 +122,13 @@ void traverse(Node* root) {
                 cout << "3";
                 break;
             case 4:
-                cout << "4";
+                cout << "Stooping down to pick up the mushroom. You get a sharp pungent smell of burnt orange peals. Your head snaps back as a wave of euphoria washes over you. A few seconds later a deep pounding pain in your head starts. Placing the mushroom down you drink some water and the pain fades. Feeling like yourself you contemplate if eating the mushroom would produce more euphoric effects. A strong urge to eat the mushroom washes over you. Do you give into your urges  (6) or do you continue along your path. (2).";
                 break;
             case 5:
                 cout << "5";
                 break;
             case 6:
-                cout << "6";
+                cout << "You pick the mushroom up and take a large bite. As expected the feeling of euphoria crushes you knocking you to the ground. Sudden strong hallucinations of a large spider standing over you speaking to you in rhymes fill your vision. Frightened you scream and crawl off the path deep into the forest. You are so unaware of where you are going that you don’t notice the steep hill ahead of you. You fall down the hill rolling and tumbling until your head hits a tree and your vision goes dark. You snap awake, body halfway in a river, head pounding. Do you drink the water unknowing its sanitation (5) or do you attempt to find your water canteen  (7)?";
                 break;
             case 7:
                 cout << "7";
